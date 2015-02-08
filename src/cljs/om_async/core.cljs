c98459efb5a3fbd4c3b1b440dac931acc81db8af
Sebastian Bensusan <sbensu@gmail.com>
2015-02-08 20:12:20 +0100

    First part, up to editable

--- src/cljs/om_async/core.cljs
+++ src/cljs/om_async/core.cljs
@@ -14,3 +14,90 @@
 (println "Hello world!")
 
 (fw/start {:websocket-url "ws://localhost:3449/figwheel-ws"})
+
+(def ^:private meths
+  {:get "GET"
+   :put "PUT"
+   :post "POST"
+   :delete "DELETE"})
+
+(defn edn-xhr [{:keys [method url data on-complete]}]
+  (let [xhr (XhrIo.)]
+    (events/listen xhr goog.net.EventType.COMPLETE
+      (fn [e]
+        (on-complete (reader/read-string (.getResponseText xhr)))))
+    (. xhr
+      (send url (meths method) (when data (pr-str data))
+        #js {"Content-Type" "application/edn"}))))
+
+(def app-state
+  (atom {:classes []}))
+
+(defn display [show]
+  (if show
+    #js {}
+    #js {:display "none"}))
+
+(defn handle-change [e data edit-key owner]
+  (om/transact! data edit-key (fn [_] (.. e -target -value))))
+
+(defn end-edit [text owner cb]
+  (om/set-state! owner :editing false)
+  (cb text))
+
+(defn editable [data owner {:keys [edit-key on-edit] :as opts}]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:editing false})
+    om/IRenderState
+    (render-state [_ {:keys [editing]}]
+      (let [text (get data edit-key)]
+        (dom/li nil
+          (dom/span #js {:style (display (not editing))} text)
+          (dom/input
+            #js {:style (display editing)
+                 :value text
+                 :onChange #(handle-change % data edit-key owner)
+                 :onKeyPress #(when (== (.-keyCode %) 13)
+                                (end-edit text owner on-edit))
+                 :onBlur (fn [e]
+                           (when (om/get-state owner :editing)
+                             (end-edit text owner on-edit)))})
+          (dom/button
+            #js {:style (display (not editing))
+                 :onClick #(om/set-state! owner :editing true)}
+            "Edit"))))))
+
+(defn on-edit [id title]
+  (edn-xhr
+    {:method :put
+     :url (str "class/" id "/update")
+     :data {:class/title title}
+     :on-complete
+     (fn [res]
+       (println "server response:" res))}))
+
+(defn classes-view [app owner]
+  (reify
+    om/IWillMount
+    (will-mount [_]
+      (edn-xhr
+        {:method :get
+         :url "classes"
+         :on-complete #(om/transact! app :classes (fn [_] %))}))
+    om/IRender
+    (render [_]
+      (dom/div #js {:id "classes"}
+        (dom/h2 nil "Classes")
+        (apply dom/ul nil
+          (map
+            (fn [class]
+              (let [id (:class/id class)]
+                (om/build editable class
+                  {:opts {:edit-key :class/title
+                          :on-edit #(on-edit id %)}})))
+            (:classes app)))))))
+
+(om/root classes-view app-state
+  {:target (gdom/getElement "classes")})
